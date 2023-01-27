eb39f8e19ccd8e6b4a50fed1bff9a1aafc5ddb10
David Schramm <david.schramm@live.com>
2023-01-27 07:34:37 +0100

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,68 @@
 mod temper;
 
+use std::sync::{Arc, Mutex};
+use std::{thread, time};
+use std::process;
+use std::env;
 
-fn main() {
-	let t=temper::Temper::new(9);
+use tide::Request;
 
-	dbg!(t);
+const UPDATE_INTERVAL:time::Duration = time::Duration::from_secs(3);
+
+#[derive(Clone)]
+struct State {
+    temp: Arc<Mutex<Vec<f32>>>
+}
+
+#[async_std::main]
+async fn main() ->  tide::Result<()> {
+    let temper_instance=temper::Temper::new().unwrap();
+
+    let mut sticks=temper_instance.get_sticks();
+
+    for i in 0..sticks.len(){
+        if let Err(e)=sticks[i].init(){
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }else{
+           println!("{}", sticks[i].get_temp().unwrap());
+        }
+    }
+    
+    let state=State{temp: Arc::new(Mutex::new(Vec::with_capacity(sticks.len())))};
+
+
+    let mut app = tide::with_state(state.clone());
+
+    app.at("/").get(|req: Request<State>| async move {
+        let p=req.state().clone();
+        let mut msg=String::new();
+        
+        {
+            let temps=p.temp.lock().unwrap();
+            for i in 0..temps.len() {
+                msg.push_str(&format!("temp{}: {}", i, temps[i]));
+            }
+        }
+
+        Ok(msg)
+    });
+
+    thread::spawn(move || {
+                loop {        
+                    {
+                        let p=state.temp.clone();
+                        let mut temp = p.lock().unwrap();
+                        for i in 0..sticks.len() {
+                            temp[i]=sticks[i].get_temp().unwrap();
+                        }
+                    }
+                    thread::sleep(UPDATE_INTERVAL);
+                }
+            });
+
+    let listen_addr=format!("0.0.0.0:{}", { if env::args().len()>1 { env::args().nth(2).unwrap() } else { String::from("8777") }});
+    app.listen(listen_addr).await?;
+    
+    Ok(())
 }
--- src/temper.rs
+++ src/temper.rs
@@ -1,23 +1,106 @@
 use rusb::{
 	Context,
 	UsbContext,
-	devices,
 	Device,
 	DeviceHandle,
 	Error
 };
 
-pub struct Temper {
-	ctx: rusb::Context,
-}
+use core::time::Duration;
 
-pub struct TemperStick <T: UsbContext> {
-	dev: Device<T>,
-	handle: DeviceHandle<T>
-}
 
 const TEMPER_VENDOR:u16 = 0x0c45;
 const TEMPER_PRODUCT:u16 = 0x7401;
+const TIMEOUT:Duration = Duration::new(5,0);
+
+pub struct TemperStick<T: UsbContext>  {
+	handle: DeviceHandle<T>
+}
+
+impl<T: UsbContext>  TemperStick<T> {
+    fn new(dev: Device<T>) -> Self{
+        TemperStick{handle:(&dev).open().unwrap()}
+    }
+
+    pub fn init(&mut self) -> Result<(), Error>{
+        for i in 0..2 {
+            match self.handle.detach_kernel_driver(i){
+                Ok(()) => (),
+                Err(_) => eprintln!("kernel driver already detached...")
+            }
+        }
+       
+        self.handle.set_active_configuration(1)?;
+
+        for i in 0..2 {
+            self.handle.claim_interface(i)?
+        }
+        
+        self.handle.write_control(0x21, 0x09, 0x0201, 0x00, &[0x01, 0x01], TIMEOUT)?;
+        
+        
+        let init_msgs:[[u8; 8]; 3]=[[0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00],
+                                    [0x01, 0x82, 0x77, 0x01, 0x00, 0x00, 0x00, 0x00],
+                                    [0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00]];
+        
+        let mut buf=[0u8;8];
+        
+        for i in 0..3 {
+            self.handle.write_control(0x21, 0x09, 0x0200, 0x01, &init_msgs[i], TIMEOUT)?;
+            
+            match self.handle.read_interrupt(0x82, &mut buf, TIMEOUT) {
+                Ok(8) => (),
+                Ok(x) => { dbg!("only read {} bytes!", x); return Err(Error::Other) },
+                Err(e) => return Err(e)
+            }
+        }
+	
+
+        match self.handle.read_interrupt(0x82, &mut buf, TIMEOUT) {
+            Ok(8) => (),
+            Ok(x) => { dbg!("only read {} bytes!", x); return Err(Error::Other) },
+            Err(e) => return Err(e)
+        }
+
+        Ok(())
+    }
+    
+        
+
+    pub fn get_temp(&self) -> Result<f32, Error> {
+        let mut msg:[u8; 8]=[0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00];
+            
+
+        self.handle.write_control(0x21, 0x09, 0x0200, 0x01, &msg, TIMEOUT)?;
+
+	    msg=[0u8; 8];
+
+        match self.handle.read_interrupt(0x82, &mut msg, TIMEOUT) {
+            Ok(8) => (),
+            Ok(x) => { dbg!("only read {} bytes!", x); return Err(Error::Other) },
+            Err(e) => return Err(e)
+        }
+
+    	let temp_i=(msg[3] as i32) + ((msg[2] as i32)<<8);
+
+        let temp:f32=(temp_i as f32)*(125.0/32000.0);
+
+        Ok(temp)
+    }
+}
+
+impl<T: UsbContext> Drop for TemperStick<T> {
+    fn drop(&mut self) {
+        for i in 0..2 {
+            self.handle.release_interface(i).ok();
+            self.handle.attach_kernel_driver(i).ok();
+        }
+    }
+}
+
+pub struct Temper {
+	ctx: rusb::Context,
+}
 
 impl Temper {
 	pub fn new()->Result<Self, ()> {
@@ -28,14 +111,12 @@ impl Temper {
 		}
 	}
 
-	pub fn count_sticks(&self) -> Result<usize, Error> {
-		Ok(self.ctx.devices()?.iter().filter(|x| {
-				let dev_desc=x.device_descriptor().unwrap();
+	pub fn get_sticks(&self) -> Vec<TemperStick<Context>> {
+        let sticks:Vec<TemperStick<Context>>=self.ctx.devices().unwrap().iter().filter(|x| {
+            	let dev_desc=x.device_descriptor().unwrap();
 				dev_desc.vendor_id()==TEMPER_VENDOR && dev_desc.product_id()==TEMPER_PRODUCT
-			}).count())
-	}
-
-	pub fn get_sticks(&self) -> Vec<TemperStick<UsbContext>> {
-	
+        }).map(|x| TemperStick::new(x)).collect();
+           
+        sticks
 	}
 }
