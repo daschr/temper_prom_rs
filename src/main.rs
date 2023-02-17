e7e5031c498eab5d51fd80479af9d839a704b90d
David Schramm <david.schramm@live.com>
2023-02-17 21:36:03 +0100

    use rwlock

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,64 @@
 mod temper;
 
-use std::sync::{Arc, Mutex};
-use std::{thread, time};
-use std::process;
 use std::env;
+use std::process;
+use std::sync::{Arc, RwLock};
+use std::{thread, time};
 
 use tide::Request;
 
-const UPDATE_INTERVAL:time::Duration = time::Duration::from_secs(3);
+const UPDATE_INTERVAL: time::Duration = time::Duration::from_secs(3);
+
+const HEADER: &str = "# HELP temper_temp The temperature measured by a TEMPer USB stick
+# TYPE temper_temp gauge\n";
 
 #[derive(Clone)]
 struct State {
-    temp: Arc<Mutex<Vec<f32>>>
+    temp: Arc<RwLock<Vec<f32>>>,
 }
 
 #[async_std::main]
-async fn main() ->  tide::Result<()> {
-    let temper_instance=temper::Temper::new().unwrap();
+async fn main() -> tide::Result<()> {
+    let temper_instance = temper::Temper::new().unwrap();
 
-    let mut sticks=temper_instance.get_sticks();
+    let mut sticks = temper_instance.get_sticks();
 
-    for i in 0..sticks.len(){
-        if let Err(e)=sticks[i].init(){
+    for i in 0..sticks.len() {
+        if let Err(e) = sticks[i].init() {
             eprintln!("Error: {}", e);
             process::exit(1);
-        }else{
-           println!("{}", sticks[i].get_temp().unwrap());
+        } else {
+            println!("{}", sticks[i].get_temp().unwrap());
         }
     }
-    
-    let state=State{temp: Arc::new(Mutex::new(Vec::with_capacity(sticks.len())))};
 
-    state.temp.clone().lock().unwrap().resize(sticks.len(), 0.0f32);
+    let state = State {
+        temp: Arc::new(RwLock::new(Vec::with_capacity(sticks.len()))),
+    };
+
+    state
+        .temp
+        .clone()
+        .write()
+        .unwrap()
+        .resize(sticks.len(), 0.0f32);
 
     let mut app = tide::with_state(state.clone());
 
     app.at("/").get(|req: Request<State>| async move {
-        let p=req.state().clone();
-        let mut msg=String::from("# HELP temper_temp The temperature measured by a TEMPer USB stick\n\
-                                 # TYPE temper_temp gauge\n");
+        let p = req.state().clone();
+        let mut msg = String::from(HEADER);
 
         {
-            let temps=p.temp.lock().unwrap();
-            let temps_len=temps.len();
+            let temps = p.temp.read().unwrap();
+            let temps_len = temps.len();
 
             for i in 0..temps_len {
-                msg.push_str(&format!("temper_temp{{stick=\"{}\"}} {}\n", temps_len-i-1, temps[i]));
+                msg.push_str(&format!(
+                    "temper_temp{{stick=\"{}\"}} {}\n",
+                    temps_len - i - 1,
+                    temps[i]
+                ));
             }
         }
 
@@ -53,21 +66,27 @@ async fn main() ->  tide::Result<()> {
     });
 
     thread::spawn(move || {
-                loop {        
-                    {
-                        let p=state.temp.clone();
-                        let mut temp = p.lock().unwrap();
-                        for i in 0..sticks.len() {
-                            temp[i]=sticks[i].get_temp().unwrap();
-                        }
-                    }
-                    thread::sleep(UPDATE_INTERVAL);
+        let p = state.temp.clone();
+        loop {
+            {
+                let mut temp = p.write().unwrap();
+                for i in 0..sticks.len() {
+                    temp[i] = sticks[i].get_temp().unwrap();
                 }
-            });
+            }
+            thread::sleep(UPDATE_INTERVAL);
+        }
+    });
 
-    let listen_addr=format!("0.0.0.0:{}", { if env::args().len()>1 { env::args().nth(1).unwrap() } else { String::from("9177") }});
+    let listen_addr = format!("0.0.0.0:{}", {
+        if env::args().len() > 1 {
+            env::args().nth(1).unwrap()
+        } else {
+            String::from("9177")
+        }
+    });
 
     app.listen(listen_addr).await?;
-    
+
     Ok(())
 }
